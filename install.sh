88f38181580264e1f0b01743c22e72d9180bb7cf
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2024-05-02 20:28:54 +0700

    Rebuild

--- install.sh
+++ install.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
-# RTASERVER.
-
-DIR=/tmp
 
 clear
-echo ""
-echo -e "\e[1;35m========== OpenWrt Rakitan Manager ==========\e[0m"
-echo -e "\e[1;35m=========== Auto Script Installer ===========\e[0m"
-echo ""
 
-install_update(){
-    echo "Update and install prerequisites"
+#===================
+W='\e[1;37m' # Putih
+R='\e[31;1m' # Merah
+G='\e[32;1m' # Hijau
+Y='\e[33;1m' # Kuning
+DB='\e[34;1m' # Biru Gelap
+P='\e[35;1m' # Ungu
+LB='\e[36;1m' # Biru Terang
+#=====================
+
+finish(){
+    clear
+    echo ""
+    echo -e "${G}====================================${W}"
+    echo -e "${G}========= INSTALL BERHASIL =========${W}"
+    echo -e "${G}====================================${W}"
+    echo ""
+    echo -e "${Y}Silahkan Cek Di Tab Modem Dan Pilih Rakitan Manager${W}"
+    echo -e "${Y}Jika Tidak Ada Silahkan Clear Cache Kemudian Logout Dan Login Kembali${W}"
+    echo -e "${Y}Atau Membuka Manual Di Tab Baru : 192.168.1.1/rakitanmanager${W}"
+    echo ""
+    echo -e "${Y}Ulangi Instalasi Jika Ada Yang Gagal :)"
+    read -n 1 -s -r -p "${Y}Ketik Apapun Untuk Kembali Ke Menu${W}"
+    bash -c "$(wget -qO - 'https://raw.githubusercontent.com/rtaserver/RakitanManager/main/install.sh')"
+}
+
+install_upgrade()
+{
+    echo "Update dan instal prerequisites"
     clear
     opkg update
     sleep 1
@@ -36,57 +56,31 @@ install_update(){
         if ! pip3 show requests >/dev/null; then
             echo "Installing package 'requests'"
             if ! pip3 install requests; then
-                echo -e "\e[1;31mError installing package 'requests'\e[0m"
-                echo -e "\e[1;31mSetup Gagal | Mohon Coba Kembali\e[0m"
+                echo -e "${R}Error installing package 'requests'${W}"
+                echo -e "${R}Setup Gagal | Mohon Coba Kembali${W}"
                 exit  # Keluar dari skrip dengan status error
             fi
         else
-            echo -e "\e[1;32mPackage 'requests' already installed\e[0m"
+            echo -e "${G}Package 'requests' sudah terinstal${W}"
         fi
 
         # Instal paket 'huawei-lte-api' jika belum terinstal
         if ! pip3 show huawei-lte-api >/dev/null; then
             echo "Installing package 'huawei-lte-api'"
             if ! pip3 install huawei-lte-api; then
-                echo -e "\e[1;31mError installing package 'huawei-lte-api'\e[0m"
-                echo -e "\e[1;31mSetup Gagal | Mohon Coba Kembali\e[0m"
+                echo -e "${R}Error installing package 'huawei-lte-api'${W}"
+                echo -e "${R}Setup Gagal | Mohon Coba Kembali${W}"
                 exit  # Keluar dari skrip dengan status error
             fi
         else
-            echo -e "\e[1;32mPackage 'huawei-lte-api' already installed\e[0m"
+            echo -e "${G}Package 'huawei-lte-api' sudah terinstal${W}"
         fi
     else
-        echo -e "\e[1;31mError: 'pip3' command not found\e[0m"
-        echo -e "\e[1;31mSetup Gagal | Mohon Coba Kembali\e[0m"
+        echo -e "${R}Error: 'pip3' command tidak ditemukan${W}"
+        echo -e "${R}Setup Gagal | Mohon Coba Kembali${W}"
         exit  # Keluar dari skrip dengan status error
     fi
     sleep 1
-}
-
-finish(){
-    clear
-    echo ""
-    echo -e "\e[1;32m====================================\e[0m"
-    echo -e "\e[1;32m========= INSTALL BERHASIL =========\e[0m"
-    echo -e "\e[1;32m====================================\e[0m"
-    echo ""
-    echo -e "\e[1;33mSilahkan Cek Di Tab Modem Dan Pilih Rakitan Manager\e[0m"
-    echo -e "\e[1;33mJika Tidak Ada Silahkan Clear Cache Kemudian Logout Dan Login Kembali\e[0m"
-    echo -e "\e[1;33mAtau Membuka Manual Di Tab Baru : 192.168.1.1/rakitanmanager\e[0m"
-    echo ""
-    echo -e "\e[1;33mUlangi Instalasi ? Jika Ada Yang Gagal\e[0m"
-    echo -e "\e[1;33m[Y] Untuk Mengulang Installasi\e[0m"
-    echo -e "\e[1;33m[N] Untuk Menyelesaikan Script\e[0m"
-    read -p "(y/n)? " yn
-    case $yn in
-        [Yy]* ) install_update; break;;
-        [Nn]* ) exit;;
-        * ) echo -e "\e[1;31mMohon Hanya Masukan 'y' atau 'n'.\e[0m";;
-    esac
-}
-
-download_files()
-{
     clear
     echo "Downloading files from repo..."
     rakitanmanager_api="https://api.github.com/repos/rtaserver/RakitanManager/releases"
@@ -94,53 +88,55 @@ download_files()
     if [ -f "$DIR/rakitanmanager.ipk" ]; then
         rm -f $DIR/rakitanmanager.ipk
     fi
-    if [ $1 == "1" ]; then
-        rakitanmanager_file_down="$(curl -s ${rakitanmanager_api} | grep "browser_download_url" | grep -oE "https.*${rakitanmanager_file}.*_21_02.ipk" | head -n 1)"
-        wget -O $DIR/rakitanmanager.ipk ${rakitanmanager_file_down}
-        opkg install $DIR/rakitanmanager.ipk --force-reinstall
-        sleep 3
-    fi
-    if [ $1 == "2" ]; then
-        rakitanmanager_file_down="$(curl -s ${rakitanmanager_api} | grep "browser_download_url" | grep -oE "https.*${rakitanmanager_file}.*_23_05.ipk" | head -n 1)"
-        wget -O $DIR/rakitanmanager.ipk ${rakitanmanager_file_down}
-        opkg install $DIR/rakitanmanager.ipk --force-reinstall
-        sleep 3
-    fi
-
+    rakitanmanager_file_down="$(curl -s ${rakitanmanager_api} | grep "browser_download_url" | grep -oE "https.*${rakitanmanager_file}.*.ipk" | head -n 1)"
+    wget -O $DIR/rakitanmanager.ipk ${rakitanmanager_file_down}
+    opkg install $DIR/rakitanmanager.ipk --force-reinstall
+    sleep 3
     finish
 }
 
-echo ""
-while true; do
-    echo "============================================"
-    echo "Install Beberapa Dependens yang di Butukan."
-    echo ""
-    echo -e "\e[1;36m[Y]\e[0m Untuk Install"
-    echo -e "\e[1;36m[N]\e[0m Untuk Keluar Dari Script"
-    echo "============================================"
-    read -p "(y/n)? " yn
-    case $yn in
-        [Yy]* ) install_update; break;;
-        [Nn]* ) exit;;
-        * ) echo -e "\e[1;31mMohon Hanya Masukan 'y' atau 'n'.\e[0m";;
-    esac
-done
+uninstaller() {
+	echo "Menghapus Rakitan Manager"
+	opkg remove luci-app-rakitanmanager
+	clear
+	echo "Menghapus Rakitan Manager Selesai"
+	read -n 1 -s -r -p "${Y}Ketik Apapun Untuk Kembali Ke Menu${W}"
+	bash -c "$(wget -qO - 'https://raw.githubusercontent.com/rtaserver/RakitanManager/main/install.sh')"
+}
 
-echo ""
+echo -e "${DB} =================================================="
+echo -e "${R}          RAKITAN MANAGER AUTO INSTALLER           "
+echo -e "${DB} =================================================="
+echo -e "${R} Versi Terinstall:     "
+echo -e "${R} Versi Terbaru:     "
+echo -e "${DB} =================================================="
+echo -e "${LB} DAFTAR MENU :                                     "
+echo -e "${LB} [\e[36m1\e[0m${LB}] Install / Upgrade Rakitan Manager                        "
+echo -e "${LB} [\e[36m2\e[0m${LB}] Uninstall Rakitan Manager                            "
+echo -e "${DB} =================================================="
+echo -e "${W}"
+echo -e   ""
+echo -e   " Ketik [ x ] Atau [ Ctrl+C ] Untuk Keluar Dari Script"
+read -p " Pilih Menu :  "  opt
+echo -e   ""
 
-while true; do
-    echo "============================================"
-    echo "Download Dan Install IPK RakitanManager."
-    echo ""
-    echo -e "\e[1;36m[1]\e[0m Untuk Versi OpenWrt 21.02 Ke Atas Kecuali 23.05"
-    echo -e "\e[1;36m[2]\e[0m Untuk Versi OpenWrt 23.05 Ke Atas"
-    echo -e "\e[1;36m[X]\e[0m Untuk Keluar Dari Script"
-    echo "============================================"
-    read -p "(1/2/x)? " version
-    case $version in
-        [1]* ) download_files "1"; break;;
-        [2]* ) download_files "2"; break;;
-        [Xx]* ) exit;;
-        * ) echo -e "\e[1;31mMohon Hanya Masukan '1' '2' Atau 'X'.\e[0m";;
-    esac
-done
+case $opt in
+1) clear ;
+echo -e Proses Install / Upgrade Akan Di Jalankan, mohon ditunggu
+echo -e
+sleep 3
+clear
+install_upgrade
+ ;;
+
+2) clear ;
+echo -e Proses Uninstall Rakitan Manager, mohon ditunggu
+echo -e
+sleep 3
+clear
+uninstaller
+ ;;
+
+x) exit ;;
+*) echo "Anda salah tekan " ; sleep 1 ; bash -c "$(wget -qO - 'https://raw.githubusercontent.com/rtaserver/RakitanManager/main/install.sh')" ;;
+esac
