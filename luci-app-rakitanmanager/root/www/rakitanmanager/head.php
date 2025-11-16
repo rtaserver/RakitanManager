b923c177883c42e6ec080c06f09d51f215231d0d
RTA SERVER <142883865+rtaserver@users.noreply.github.com>
2025-11-16 15:12:08 +0700

    `Update GitHub Actions workflow and modify frontend code for new release features`

--- luci-app-rakitanmanager/root/www/rakitanmanager/head.php
+++ luci-app-rakitanmanager/root/www/rakitanmanager/head.php
@@ -1,10 +1,11 @@
 <!-- Required meta tags -->
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="css/font-awesome.min.css">
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="lib/vendor/bootstrap/css/bootstrap.min.css">
 <!-- Main CSS -->
-<link rel="stylesheet" href="css/style.css">
 
 <link rel="icon" type="image/png" href="img/icon1.png">
-<title>Modem Rakitan Manager | <?= $title ?? 'Dashboard' ?></title>
+<title>Modem Rakitan Manager | <?= $title ?></title>
